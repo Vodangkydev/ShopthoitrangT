08b9168ca6ae7e660dff6b7eee2aa51b8013f19d
Vodangkydev <165755675+Vodangkydev@users.noreply.github.com>
2024-05-07 01:35:56 +0700

    Update MyConstants.js

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,12 +1,12 @@
 const MyConstants = {
-    DB_SERVER: 'cluster0.b6b9ukd.mongodb.net',
-    DB_USER: 'haivippow',
-    DB_PASS: 'haivippow',
+    DB_SERVER: 'cluster0.fusxwsf.mongodb.net',
+    DB_USER: 'vodangky',
+    DB_PASS: '030102Ky',
     DB_DATABASE: 't_shop',
-    JWT_SECRET: 'hai123',
+    JWT_SECRET: 'uchiha',
     JWT_EXPIRES: '36000000', // in milliseconds
-    EMAIL_USER: 'shophaipbe12@gmail.com', // gmail service
-    EMAIL_PASS: 'jetlbhgdpkszljzw',
+    EMAIL_USER: 'vodangky.it@gmail.com', // gmail service
+    EMAIL_PASS: 'upyr dmni tkip ahgl',
 
   };
-  module.exports = MyConstants;
+ 
